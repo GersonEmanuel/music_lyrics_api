df8be9325c6ce29ee3299a634028d9c482ed3d0a
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-07 15:43:21 -0300

    using only terminal

--- app.py
+++ app.py
@@ -5,18 +5,43 @@ import os
 def search_lyrics(banda, music):
     endpoint = f'https://api.lyrics.ovh/v1/{banda}/{music}'
     response = requests.get(endpoint)
-    return response.json()['lyrics'] if response.status_code == 200 else ''
-st.title('Music lyrics')
-banda = st.text_input('Artist name or band name:', key = 'banda')
-music = st.text_input('Music name', key='music')
-search = st.button('Search')
-if search:
-    lyrics = search_lyrics(banda, music)
+    return response.json()['lyrics'] if response.status_code == 200 else None
+
+def validing(question):
+    if question not in ['yes', 'no']:
+        return False
+    return True
+
+def new_lyrics():
+    again = str(input("would you like search a new lyrics? [yes/no]: "))
+    try:
+        if not validing(again):
+            raise ValueError
+    except ValueError:
+        print('only yes or no')
+        new_lyrics()
+
+    if again == 'yes':
+        return True
+    return False
+
+def main():
+    band = str(input('Artist name or band name: '))
+    music = str(input('Music name'))
+    lyrics = search_lyrics(band, music)
     if lyrics:
-        st.success('lyrics found ')
-        st.text(f'Music: {music}')
-        st.text(lyrics)
+        print('lyrics found')
+        print(f'music: {music}')
+        print(lyrics)
+        print('-'*100)
+
     else:
-        st.error('lyrics not found, try again')
-    
+        print('lyrics not found')
+
+    if new_lyrics():
+        main()
+    print('end')
+
+main()
+
 
